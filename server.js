29af98eeb8e079d256d38dd9d19dfdd103468b73
Duncan Marsh <dwolfm@gmail.com>
2016-05-25 00:03:34 -0700

    no mongo

--- server.js
+++ server.js
@@ -1,14 +1,23 @@
 'use strict';
 
-const port = process.env.PORT || 3000;
-const baseStorageDir = process.env.STORAGE_DIR || `${__dirname}/data`;
+// node modules
 const express = require('express');
+const debug = require('debug')('demo:server');
+
+// app modules
 const Storage = require('./lib/storage');
 const errorResponse = require('./lib/error-response');
-const appStorage = new Storage(baseStorageDir);
+
+// globals
 const app = express();
+const port = process.env.PORT || 3000;
+const baseStorageDir = process.env.STORAGE_DIR || `${__dirname}/data`;
+const appStorage = new Storage(baseStorageDir);
+
+// middleware
 app.use(errorResponse);
 
+// routes
 const userRouter = require('./route/user-router')(appStorage);
 app.use('/api/user', userRouter);
 
@@ -19,10 +28,13 @@ const listRouter = require('./route/list-router')(appStorage);
 app.use('/api/list', listRouter);
 
 app.get('*', function(req, res){
+  debug('* 404')
   res.json('404 not found');
 });
 
+// start app
 const server = app.listen(port, function(){
+  debug('app.listen');
   console.log('server up ->', port);
 });
 
